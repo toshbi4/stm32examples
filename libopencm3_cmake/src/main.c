f7057b2e1cb13fc9f7752876a182908de1528773
AntonPotapov <ant.potapoff3000@yandex.ru>
2020-11-07 22:19:39 +0300

    Fix style

--- libopencm3_cmake/src/main.c
+++ libopencm3_cmake/src/main.c
@@ -1,13 +1,9 @@
-//
-// Created by sl on 26.02.17.
-//
-
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <stdio.h>
 
-// PB7 - LED
+// PB4 - LED
 
 static void clock_setup(void) {
   rcc_clock_setup_in_hse_8mhz_out_72mhz();
@@ -21,11 +17,11 @@ static void gpio_setup(void) {
   
   // LED
   gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-                GPIO_CNF_OUTPUT_PUSHPULL, GPIO7);
+                GPIO_CNF_OUTPUT_PUSHPULL, GPIO4);
 
   AFIO_MAPR |= AFIO_MAPR_SWJ_CFG_FULL_SWJ_NO_JNTRST;
 
-  gpio_set(GPIOB, GPIO7);
+  gpio_set(GPIOB, GPIO4);
   // LED END
 }
 
@@ -33,7 +29,7 @@ int main(void) {
   clock_setup();
   gpio_setup();
 
-  gpio_set(GPIOB, GPIO7);
+  gpio_set(GPIOB, GPIO4);
 
   while(1) {
 
